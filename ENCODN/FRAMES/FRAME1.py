8e737adb2a9e0e506ccaa469de8d36dfbd6bccc8
akshitadixit <56997545+akshitadixit@users.noreply.github.com>
2020-10-10 16:36:54 +0530

    Update FRAME1.py

--- ENCODN/FRAMES/FRAME1.py
+++ ENCODN/FRAMES/FRAME1.py
@@ -1,1 +1,8 @@
+#name of the frame is: Frame1
+from tkinter import *
 
+class FRAME1(Frame)
+  def __init__(self, master=None):
+    Frame.__init__(self, master)
+    Frame1 = Frame(master, bg="black")
+    Frame1.grid(row=0, column=0, padx=5, pady=5)
