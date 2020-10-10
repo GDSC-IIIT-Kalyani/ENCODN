a6deaceb5adc2151b3cfac69d6555d4686aa89ce
akshitadixit <56997545+akshitadixit@users.noreply.github.com>
2020-10-10 17:10:52 +0530

    Update FRAME1.py

--- ENCODN/FRAMES/FRAME1.py
+++ ENCODN/FRAMES/FRAME1.py
@@ -1,9 +1,10 @@
-#name of the frame is: Frame1
+#name of the frame is: FRAME1
 from tkinter import *
 import ENCODN
+import FRAME2 as fr2
 
-class FRAME1(Frame)
+class FRAME1class(Frame)
   def __init__(self, master=None):
     Frame.__init__(self, master)
-    Frame1 = Frame(master, bg="black")
-    Frame1.grid(row=0, column=0, padx=5, pady=5)
+    FRAME1 = Frame(master, bg="black")
+    FRAME1.grid(row=0, column=0, padx=5, pady=5)
