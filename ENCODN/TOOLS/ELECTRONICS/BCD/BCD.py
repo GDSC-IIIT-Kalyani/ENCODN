7e91cccc09002a341b158350795c8d797a2b8466
Jovaun McGregor <jovaun.mcg@gmail.com>
2020-10-12 14:04:03 +0000

    Add basic input validation for BCD encrypt and decrypt

--- ENCODN/TOOLS/ELECTRONICS/BCD/BCD.py
+++ ENCODN/TOOLS/ELECTRONICS/BCD/BCD.py
@@ -1,3 +1,4 @@
+import re
 
 # Converts string of decimals to a BCD string
 def encrypt(string):
@@ -10,17 +11,27 @@ def encrypt(string):
             dec //= 2
     except ValueError:
         print("The entered value is incorrect!!!")
-        print("Unable to convert String to Decimal")
+        return None
     return cipher[::-1]
 
 # Input : BCD string
 # Output: Decimal string
 def decrypt(cipher):
     decipher = 0
-    # Decryption starting from LSB
-    cipher = cipher[::-1]
-    for i in range(len(cipher)):
-        bit = int(cipher[i])
-        bit_value = bit * (2**i)
-        decipher += bit_value
+    try:
+        # Check if cipher is a binary string
+        char_Re = re.compile(r'[^0-1.]')
+        is_binary = not bool(char_Re.search(cipher))
+        if is_binary:
+            # Decryption starting from LSB
+            cipher = cipher[::-1]
+            for i in range(len(cipher)):
+                bit = int(cipher[i])
+                bit_value = bit * (2**i)
+                decipher += bit_value
+        else:
+            raise ValueError
+    except ValueError:
+        print("The entered value is incorrect!!!")
+        return None
     return str(decipher)
